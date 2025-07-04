d2b40e4e112d2cbb0c2c5b2e34995f913c36fcd6
div-j <divinejohn65@gmail.com>
2025-07-04 21:07:54 +0100

    react vite tailwind shadcn and components

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+export default function App() {
+  return (
+    <div>
+      <Button variant={"outline"} className='bg-me '>Hi</Button>
+    </div>
+  )
+}
